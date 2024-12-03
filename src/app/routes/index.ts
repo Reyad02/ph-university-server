92fda49cede8fb9ce41ceb84962905afc3a00513
unknown <areyad203011@bscse.uiu.ac.bd>
2024-12-03 21:01:08 +0600

    academic department module done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { UserRouter } from '../modules/user/user.route';
 import { StudentRouter } from '../modules/student/student.route';
 import { AcademicRouter } from '../modules/academicSemester/academicSemester.route';
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
 
 const router = Router();
 
@@ -11,6 +12,7 @@ const moduleRoutes = [
   { path: '/students', route: StudentRouter },
   { path: '/academic-semester', route: AcademicRouter },
   { path: '/academic-faculties', route: AcademicFacultyRoutes },
+  { path: '/academic-departments', route: AcademicDepartmentRoutes },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
