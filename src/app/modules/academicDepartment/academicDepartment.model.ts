b58f7cc2f30362e2dd486caf4c579e3d8325d4db
unknown <areyad203011@bscse.uiu.ac.bd>
2024-12-03 21:12:11 +0600

    new error created

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,5 +1,6 @@
 import { model, Schema } from 'mongoose';
 import { TAcademicDepartment } from './academicDepartment.interface';
+import AppError from '../../errors/AppError';
 
 const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   {
@@ -18,7 +19,7 @@ academicDepartmentSchema.pre('save', async function (next) {
   });
 
   if (isDepartmentExist) {
-    throw new Error('This department is already exist!');
+    throw new AppError(400, 'This department is already exist!');
   }
 
   next();
@@ -29,7 +30,7 @@ academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
   const isDepartmentExist = await AcademicDepartment.findOne(query);
 
   if (!isDepartmentExist) {
-    throw new Error('This department does not exist! ');
+    throw new AppError(400, 'This department does not exist! ');
   }
 
   next();
