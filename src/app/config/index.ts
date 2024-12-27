776272b7bd65340810aec6991ea96bbe4f570d5e
unknown <areyad203011@bscse.uiu.ac.bd>
2024-12-27 10:14:07 +0600

    super admin created

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -19,4 +19,5 @@ export default {
   cloudinary_cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
   cloudinary_api:process.env.CLOUDINARY_API,
   cloudinary_api_secret:process.env.CLOUDINARY_API_SECRET,
+  super_admin_pass:process.env.SUPER_ADMIN_PASS,
 };
--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -8,7 +8,7 @@ export type TUser = {
   password: string;
   needsPasswordChange: boolean;
   passwordChangedAt?: Date;
-  role: 'admin' | 'student' | 'faculty';
+  role: 'super-admin' | 'admin' | 'student' | 'faculty';
   status: 'in-progress' | 'blocked';
   isDeleted: boolean;
 };
